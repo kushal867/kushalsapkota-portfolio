45b176806bed00070e5015905555fa69106a702d
Kushal <your_email@example.com>
2025-10-03 14:32:19 +0545

    Add the admin.py

--- myport/admin.py
+++ myport/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Profile, SocialLink,SkillCategory, Skill,Category, Project, ProjectImage,Testimonial, ContactMessage
 
-# Register your models here.
+admin.site.register(Profile)
+admin.site.register(SocialLink)
+admin.site.register(SkillCategory)
+admin.site.register(Skill)
+admin.site.register(Category)
+admin.site.register(Project)
+admin.site.register(ProjectImage)
+admin.site.register(Testimonial)
+admin.site.register(ContactMessage)
